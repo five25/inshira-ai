504d4185beb9b91e9836ad4bdadd19dc69a94acb
Fabrício Aniceto de Lima <fabricio.lima@emap.ma.gov.br>
2025-04-11 16:07:05 -0300

    Correção na autenticação

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,8 @@
 import NextAuth from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
 
-import { findUserByCredentials } from './service/user-server'
 import db from './lib/db'
+import { compare } from 'bcrypt-ts'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
    providers: [
@@ -13,13 +13,23 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             password: {}
          },
          authorize: async credentials => {
-            const user = await findUserByCredentials(credentials.email as string, credentials.password as string)
+            const email = credentials.email as string
+            const password = credentials.password as string
+
+            const user = await db.user.findFirst({
+               where: { email }
+            })
+
             if (!user) return null
+
+            const passwordMatch = await compare(password, user.password)
+
+            if (!passwordMatch) return null
 
             return {
                id: user.id,
-               email: user.email,
-               name: user.name
+               name: user.name,
+               email: user.email
             }
          }
       })
